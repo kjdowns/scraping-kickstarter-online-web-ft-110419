508533ddd7adc9f28abb3eb5562852df940298cc
Kevin Downs <kevin.downs04@gmail.com>
2019-11-23 00:15:48 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,14 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
+  projects
   binding.pry
 end
 
@@ -15,4 +23,4 @@ create_project_hash
 #image link project.css("div.project-thumbnail a img").attribute("src").value
 #description project.css("p.bbcard_blurb").text
 #location project.css("span.location-name").text
-#percent funded project.css("ul.project-stats li.first.funded strong").text
+#percent funded project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
